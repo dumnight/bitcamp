8afe5ee542c53934775d76449d7ca87937e96aab
Haeseong <dumnight@gmail.com>
2017-09-26 17:24:43 +0900

    '.'

---
+++ java-basic/exsamples/Test11_11.java
@@ -0,0 +1,17 @@
+// # 자바에서 값 표현(literal)하기 - 논리 값 표현
+package bitcamp.java100;
+
+public class Test11_11 {
+    public static void main(String[] args){
+       
+        System.out.println(true);
+        System.out.println(false);
+       // System.out.println(True);
+       // System.out.println(0 == false);
+       
+
+
+
+    }
+}
+
---
+++ java-basic/exsamples/Test11_9.java
@@ -0,0 +1,12 @@
+// # 자바에서 값 표현(literal)하기 - 부동 소수점의 크기
+package bitcamp.java100;
+
+public class Test11_9 {
+    public static void main(String[] args){
+        // 부동 소수점을 메모리에 저장할때
+
+        System.out.println(12.375); 
+        System.out.println(12.117);
+        System.out.println(214500/1.1);
+    }
+}
