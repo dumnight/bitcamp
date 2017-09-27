84ccfff5866240d116438629d9e2e3271ac82c10
Haeseong <dumnight@gmail.com>
2017-09-27 18:56:12 +0900

    '.'

--- java-basic/src/main/java/bitcamp/java100/Test13_1.java
+++ java-basic/src/main/java/bitcamp/java100/Test13_1.java
@@ -1,5 +1,5 @@
 package bitcamp.java100;
-
+/*
     public class Test13_1{
         public static void main(String args[]){
            int kor, eng, math, soc, sci, eth, chi;
@@ -19,7 +19,23 @@ package bitcamp.java100;
            scores[4] = 100;
            scores[5] = 100;
            scores[6] = 100;
-           
+
 
         }
-    }                                                                                                                                                                
+    }                                                                                   */
+    public class Test13_1{
+        public static void main(String[] args){
+            String[] names = {"홍길동", "임꺽정", "유관순"};
+            byte[][] scores = {{100,100,100},{90,90,90},{80,80,80}};
+            short[] totals = {300,270,240};
+            float[] aver = {100f,90f,80.0f};
+
+            for(int i = 0; i < 3; i++){
+                System.out.printf("%s,%d,%d,%d,%d,%.1f\n",names[i],scores[i][0]
+                ,scores[i][1],scores[i][1],totals[i],aver[i]);
+            }
+        }
+    }
+    
+ 
+                          
