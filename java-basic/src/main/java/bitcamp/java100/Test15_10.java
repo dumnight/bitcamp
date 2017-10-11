87cae02f868a6175725cbc8aff1488654c6494cf
Haeseong <dumnight@gmail.com>
2017-10-11 10:26:50 +0900

    ...

--- java-basic/src/main/java/bitcamp/java100/Test15_10.java
+++ java-basic/src/main/java/bitcamp/java100/Test15_10.java
@@ -6,8 +6,17 @@ package bitcamp.java100;
             int v1 = 0b0101_1100;
             int v2 = 0b0110_0110;
             
+            //논리곱 AND
             int result = v1 & v2;
             System.out.println(java.lang.Integer.toBinaryString(result));
-            
+            //논리합 OR
+            result = v1 & v2;
+            System.out.println(java.lang.Integer.toBinaryString(result));
+            //배타적 논리곱 XOR
+            result = v1 ^ v2;
+            System.out.println(java.lang.Integer.toBinaryString(result));
+            //부정 NOT
+            result = ~v1;
+            System.out.println(java.lang.Integer.toBinaryString(result));
         }
     }
--- java-basic/src/main/java/bitcamp/java100/Test99.java
+++ java-basic/src/main/java/bitcamp/java100/Test99.java
@@ -1,23 +1,52 @@
 package bitcamp.java100;
-/* 산술연산자(+, -, *, /, %) */
 
-
-public class Test99 {
-    public static void main(String[] args) {
-        int i = 0;
-        int r = 0;
-        
-        while (i < 10) {
-          i++;
-          if (i == 6)
-            continue; // 다음에 나오는 문장들을 실행하지 않고 바로 비교문으로 보낸다.
-          r += i;
-          System.out.printf("i = %d, r = %d\n", i, r);  
+    public class Test99{
+        public static void main(String[] args) {
+            class Score {
+                String names;
+                int kor;
+                int eng;
+                int math;
+                int sum;
+                float aver;
+                
+                void compute() {
+                    sum = kor + eng + math;
+                    aver = sum / 3.0f;
+                }
+                
+                
+                
+            }
+            Score[] scores = new Score[3];
+            scores[0] =  new Score();
+            scores[1] =  new Score();
+            scores[2] =  new Score();
+            
+            scores[0].names = "가나다";
+            scores[0].kor = 101;
+            scores[0].eng = 71;
+            scores[0].math = 91;
+            scores[0].compute();
+            
+            scores[1].names = "라마바";
+            scores[1].kor = 121;
+            scores[1].eng = 165;
+            scores[1].math = 88;
+            scores[0].compute(); 
+            
+            scores[2].names = "사아자";
+            scores[2].kor = 76;
+            scores[2].eng = 90;
+            scores[2].math = 151;
+            scores[0].compute();
+            
+            
+            for(int i =0; i < scores.length; i++) {
+                
+                //System.out.printf("%s %d %d %d %d %f\n", scores[i].names, scores[i].kor, scores[i].eng, 
+                  //      scores[i].math, scores[i].sum, scores[i].aver);
+            }
+             
         }
-        
-        System.out.printf("i = %d, r = %d\n", i, r);
-        
-        
-      }
-    }
-    
+}
