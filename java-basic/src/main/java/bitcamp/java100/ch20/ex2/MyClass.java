cbd0b18f26df90ee57fbf60b135cf8baf257e1aa
null <null>
2017-12-01 17:34:27 +0900

    '.'

---
+++ java-basic/src/main/java/bitcamp/java100/ch20/ex2/MyClass.java
@@ -0,0 +1,19 @@
+package bitcamp.java100.ch20.ex2;
+
+public class MyClass {
+    public MyClass() {
+        System.out.println("=>MyClass()");
+    }
+    public MyClass(int p1) {
+        System.out.println("=>MyClass(int)");
+    }
+    public MyClass(String p1) {
+        System.out.println("=>MyClass(Stirng)");
+    }
+    public MyClass(int p1, String p2) {
+        System.out.println("=>MyClass(int, String)");
+    }
+    public MyClass(String p1, int p2) {
+        System.out.println("=>MyClass(String, int)");
+    }
+}
