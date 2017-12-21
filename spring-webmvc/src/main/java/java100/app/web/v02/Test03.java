bd3ad241709c8070680ddee8adc827bccbc945c3
null <null>
2017-12-21 19:07:24 +0900

    '.'

---
+++ spring-webmvc/src/main/java/java100/app/web/v02/Test03.java
@@ -0,0 +1,30 @@
+package java100.app.web.v02;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/v02/test03")
+public class Test03 {
+
+    @RequestMapping("m1")
+    public String m1() {
+        System.out.println("m1()");
+        return "/hello.jsp";
+    }
+    @RequestMapping(value="m2")
+    public String m2() {
+        System.out.println("m2()");
+        return "/hello.jsp";
+    }
+    @RequestMapping(path="m3")
+    public String m3() {
+        System.out.println("m3()");
+        return "/hello.jsp";
+    }
+    @RequestMapping("/m4")
+    public String m4() {
+        System.out.println("m4()");
+        return "/hello.jsp";
+    }
+}
---
+++ spring-webmvc/src/main/webapp/hello.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Welcome</title>
+</head>
+<body>
+<h1>안녕하세요!</h1>
+<form action="/spring-webmvc/v02/test04.html" method="post">
+<button>뒤로가기</button>
+</form>
+</body>
+</html>
